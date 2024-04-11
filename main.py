b34bd26fd8be3d2f37db2aee108acf39af767cd8
Monazir Muhammad Doha <getmonazirnow@gmail.com>
2024-04-11 21:21:18 +0600

    add_length_flag

--- main.py
+++ main.py
@@ -9,30 +9,31 @@ def showProgress(nowTrying):
     stdout.write(f"\r\033[91;1mNow trying: {nowTrying} \033[0m")
     stdout.flush()
 
-q = Q()
-otpList = []
-
-for num in range(10000):
-    otp = str(num).zfill(4)
-    otpList.append(otp)
-for i in range(10000):
-    random_otp = random.choice(otpList)
-    q.put(random_otp)
-    otpList.remove(random_otp)
-    
-
-
-import argparse
-
 parser = argparse.ArgumentParser(description='Numbrute')
 
 parser.add_argument('-f', '--file', type=str, required=True, help='Path to the file')
 parser.add_argument('-e', '--error_message', type=str, help='Error message')
 parser.add_argument('-c', '--error_code', type=int, help='status code of response where the error message is shown')
 parser.add_argument('-t', '--threads', type=int, default=3, help='Number of threads')
+parser.add_argument('-l', '--length', type=int, default=4, help='Length of something')
 parser.add_argument('-H', '--disable_https', action='store_true', default=False, help='Disable HTTPS')
 
 args = parser.parse_args()
+
+q = Q()
+otpList = []
+
+for num in range(10 ** args.length):
+    otp = str(num).zfill(args.length)
+    otpList.append(otp)
+for i in range(10 ** args.length):
+    random_otp = random.choice(otpList)
+    q.put(random_otp)
+    otpList.remove(random_otp)
+    
+
+
+
 
 # Check if either error_message or error_code is provided
 if not (args.error_message or args.error_code):
